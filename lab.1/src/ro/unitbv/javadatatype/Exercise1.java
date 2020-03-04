f7168c689458be8720c10b35fb84b4455fc9da33
Zvirid Andrei <zvirida6@gmail.com>
2020-03-04 21:29:05 +0200

    first exercise

--- lab.1/src/ro/unitbv/javadatatype/Exercise1.java
+++ lab.1/src/ro/unitbv/javadatatype/Exercise1.java
@@ -1,10 +1,41 @@
 package ro.unitbv.javadatatype;
 
+import java.util.Scanner;
+
 public class Exercise1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+
+
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Introduceti primul nr:");
+		int nr1 = scanner.nextInt();
+		System.out.println("Primul numar este: "+nr1);
+		
+		System.out.println("Introduceti al doilea nr:");
+		int nr2 = scanner.nextInt();
+		System.out.println("Al doilea numar este: "+nr2);
+		
+		System.out.println("Suma este =" +(nr1+nr2));
+		System.out.println("Diferenta este =" +(nr1-nr2));
+		System.out.println("Produsul este =" +(nr1*nr2));
+		float media = (nr1+nr2)/2;
+		System.out.println("Media =" +media);
+		
+		if(nr1>nr2)
+			System.out.println("Distanta este =" +((nr1-nr2)));
+		else 
+			System.out.println("Distanta este =" +((nr2-nr1)));
+		
+		if(nr1>nr2)
+			System.out.println("Numarul maxim =" +nr1);
+		else 
+			System.out.println("Numarul maxim =" +nr2);
+			
+		
+	}
+		
 	}
 
-}
+
